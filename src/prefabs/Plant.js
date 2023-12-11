d6c157b2c636a857ec211fe9443154c206729c1c
kingmiclong <b302057069@gmail.com>
2023-12-11 11:04:58 -0800

    F1.b Internal DSL Plants refactor finished

--- src/prefabs/Plant.js
+++ src/prefabs/Plant.js
@@ -1,100 +1,85 @@
 class Plant {
     constructor(scene, x, y, species) {
         this.scene = scene;
-        this.sunlight = []; // Sunlight
-        this.water = []; // Water
-        this.species = species; // 'potato', 'tomato', or 'eggplant'
+        this.sunlight = [];
+        this.water = [];
+        this.species = species;
         this.growthStage = 0;
         this.isReadyToHarvest = false;
-
-        // Determine the frame based on the species
-        const frame = this.getFrameForSpecies(species, this.growthStage);
+        
+        const frame = PlantSpecies.getFrameForSpecies(species, this.growthStage);
         this.sprite = scene.add.sprite(x, y, 'plants', frame);
     }
 
-    // Method to update the sprite frame
     updateSprite() {
-        const frame = this.getFrameForSpecies(this.species, this.growthStage);
+        const frame = PlantSpecies.getFrameForSpecies(this.species, this.growthStage);
         this.sprite.setFrame(frame);
     }
-    
+
     grow() {
-        if (this.growthStage < 3) { // 4 grow stages including seed 1st planted
+        if (this.growthStage < 3) {
             this.growthStage++;
-            this.sprite.setFrame(this.getFrameForSpecies(this.species, this.growthStage));
-            if (this.growthStage === 3) {
-                this.isReadyToHarvest = true;
-            }
+            this.updateSprite();
+            this.isReadyToHarvest = this.growthStage === 3;
         }
     }
 
-    // Grow condition
     checkGrowthConditions() {
-        const currentSunlight = this.sunlight.reduce((a, b) => a + b, 0); // Total sunlight
-        const currentWater = this.water.reduce((a, b) => a + b, 0); // Total water
-
-        const sunNeeded = this.getSunNeeded();  
-        const waterNeeded = this.getWaterNeeded();
-
-        if (currentSunlight >= sunNeeded.min && currentSunlight <= sunNeeded.max &&
-            currentWater >= waterNeeded.min && currentWater <= waterNeeded.max) 
-        {
-            this.grow();
-        }
-        if (!sunNeeded || !waterNeeded) // Testing error and stop immediately
-        { 
-            console.error('Growth conditions are undefined for species:', this.species, 'at growth stage:', this.growthStage);
+        const speciesData = PlantSpecies.getSpeciesData(this.species);
+        if (!speciesData) {
+            console.error('Growth conditions are undefined for species:', this.species);
             return;
         }
-        // Reset the sunlight and water after checking
+
+        const currentSunlight = this.sunlight.reduce((a, b) => a + b, 0);
+        const currentWater = this.water.reduce((a, b) => a + b, 0);
+
+        if (currentSunlight >= speciesData.sun.min && currentSunlight <= speciesData.sun.max &&
+            currentWater >= speciesData.water.min && currentWater <= speciesData.water.max) {
+            this.grow();
+        }
+
         this.sunlight = [];
         this.water = [];
     }
 
-    // Sun value
-    getSunNeeded() {
-        const sunRequirements = {
-            'tomato': {min: 30, max: 40},
-            'potato': {min: 25, max: 35},
-            'eggplant': {min: 30, max: 40}
-        };
-        
-        if (!sunRequirements[this.species]) {
-            console.error('Invalid species for sun requirements:', this.species);
-            return { min: 0, max: 0 }; // or handle it differently
-        }
-    
-        return sunRequirements[this.species];
-    }
-    
-    // Water value
-    getWaterNeeded() {
-        const waterRequirements = {
-            'tomato': {min: 30, max: 40},
-            'potato': {min: 25, max: 35},
-            'eggplant': {min: 30, max: 40}
-        };
-        if (!waterRequirements[this.species]) {
-            console.error('Invalid species for sun requirements:', this.species);
-            return { min: 0, max: 0 }; // or handle it differently
-        }
-        return waterRequirements[this.species];
-    }
-
     harvest() {
-        // Reset the growth again
         this.growthStage = 0;
         this.isReadyToHarvest = false;
-        this.sprite.setFrame(this.getFrameForSpecies(this.species, this.growthStage));
+        this.updateSprite();
         console.log("Plant reset to seed stage.");
     }
+}
 
-    getFrameForSpecies(species, growthStage) {
-        const baseFrame = {
-            'potato': 0,
-            'tomato': 4,
-            'eggplant': 8
-        };
-        return baseFrame[species] + growthStage;
+class PlantSpecies {
+    static species = {
+        'potato': {
+            sun: {min: 25, max: 35},
+            water: {min: 20, max: 30},
+            frames: [0, 1, 2, 3]
+        },
+        'tomato': {
+            sun: {min: 30, max: 40},
+            water: {min: 25, max: 35},
+            frames: [4, 5, 6, 7]
+        },
+        'eggplant': {
+            sun: {min: 30, max: 40},
+            water: {min: 30, max: 40},
+            frames: [8, 9, 10, 11]
+        },
+    };
+
+    static getSpeciesData(species) {
+        return this.species[species];
+    }
+
+    static getFrameForSpecies(species, growthStage) {
+        const speciesData = this.species[species];
+        if (!speciesData || !speciesData.frames[growthStage]) {
+            console.error('Invalid species or growth stage:', species, growthStage);
+            return 0;
+        }
+        return speciesData.frames[growthStage];
     }
 }
