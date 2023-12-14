91a3a15ec514e86566fc267d433dea522d3efb09
kingmiclong <b302057069@gmail.com>
2023-12-13 19:05:40 -0800

    Testing cache

--- service-worker.js
+++ service-worker.js
@@ -4,29 +4,29 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('game-cache').then((cache) => {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/main.js',
+          './',
+          './index.html',
+          './main.js',
 
-          '/assets/en.json',
-          '/assets/ja.json',
-          '/assets/zh.json',
-          '/assets/gamemap.json',
-          '/assets/gamemap.png',
-          '/assets/gamemap.tmx',
-          '/assets/plants.json',
-          '/assets/plants.png',
-          '/assets/tiles.png',
-          '/assets/pig.png',
-          '/assets/pig.json',
-          '/assets/gameScenario.json',
+          './assets/en.json',
+          './assets/ja.json',
+          './assets/zh.json',
+          './assets/gamemap.json',
+          './assets/gamemap.png',
+          './assets/gamemap.tmx',
+          './assets/plants.json',
+          './assets/plants.png',
+          './assets/tiles.png',
+          './assets/pig.png',
+          './assets/pig.json',
+          './assets/gameScenario.json',
           
-          '/src/prefabs/Language.js',
-          '/src/prefabs/Plant.js',
+          './src/prefabs/Language.js',
+          './src/prefabs/Plant.js',
           
-          '/lib/phaser.js',
-          '/src/scenes/Load.js',
-          '/src/scenes/Play.js',
+          './lib/phaser.js',
+          './src/scenes/Load.js',
+          './src/scenes/Play.js',
         ]);
       })
     );
